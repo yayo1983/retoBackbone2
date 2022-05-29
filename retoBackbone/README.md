ba56e3bd24a95e4e7915d495991fe72bb345b0b1
yayo1983 <yazanenator@gmail.com>
2022-05-29 13:15:40 -0500

    standarizacion de salida

--- retoBackbone/README.md
+++ retoBackbone/README.md
@@ -19,16 +19,16 @@ Pasos
 2.2. Programar los seeder para el relleno de los datos según la fuente de datos en XML
 2.3. Configurar sqlite para base de datos y ejecutar migraciones
 2.4. Desarrollar lógica de negocio en el modelo
-2.5. Crear la ruta del API y la function en la clase controladora para que llame a la clase modelo que implementa el servicio
-2.6. usar git para salvar en Github
+2.5. Crear la ruta del API y la function en la clase controladora para que llame a la clase Service que implementa el servicio
+2.6. En la clase service creado funciones para standarizar la salida en el formato requerido
 
-3. Salvar proyecto en GitHub
+3. Subir proyecto en GitHub
 
 4. Publicar en AWS
-4.1. Crear instancia ubuntu e instalar paquetes y el servidor web Apache
+4.1. Crear instancia ubuntu e instalar paquetes de php y el servidor web Apache2
 4.2. Poner IP pública a la instancia
 4.3. Descargar proyecto en la instancia con git
-4.4. ejecutar php artisan optimize, config:clear and key:generate 4.5 
+4.4. ejecutar php artisan optimize, config:clear and key:generate
 
 
 
--- retoBackbone/app/Services/CPService.php
+++ retoBackbone/app/Services/CPService.php
@@ -77,11 +77,24 @@ class CPService {
         try {
             $postalCode = PostalCode::where('zip_code', $cp)->first();
             if($postalCode != null){
-                return  $postalCode->postalcode;
+                return  $this->StandarOutPut($postalCode->postalcode);
             }
         } catch (Exception $e) {
             throw $e;
         }
         return ['Not found postal code'];
     }
+
+    public function StandarOutPut (array $cp){
+        $cp['locality'] = strtoupper($this->removeAccents($cp['locality']));
+        $cp['federal_entity']->name= strtoupper($this->removeAccents($cp['federal_entity']->name));
+        return $cp;
+    }
+
+    public function removeAccents ($letters) {
+        $notPermits= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
+        $permits= array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E");
+        $letters = str_replace($notPermits, $permits ,$letters);
+        return $letters;
+        }
 }
